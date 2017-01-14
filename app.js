99dd7626d9bb88b09b06676388b0baa231a35a09
Martin Cornejo <mart256@gmail.com>
2017-01-14 20:30:19 -0300

    app.js: se cambia la localhost por ip definida

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use(parser.json());
 
 app.use('/api', router);
 
-app.listen(3000, function(){
+app.listen(3000, '192.168.50.11', function(){
 	console.log("The frontend server is running on port 3000!")
 });
